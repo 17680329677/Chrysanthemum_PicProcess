4af881053367747ce4e1303286b52ebe3c453b46
dhzazy <17680329677@163.com>
2018-11-10 09:35:08 +0800

    人工数据完成

--- pic_cv.py
+++ pic_cv.py
@@ -8,20 +8,26 @@ def crop_mum(file, name):
     img = cv2.imread(file)
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    # 用Sobel算子计算x，y方向上的梯度，之后在x方向上减去y方向上的梯度，通过这个减法，我们留下具有高水平梯度和低垂直梯度的图像区域。
     gradX = cv2.Sobel(gray_img, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
     gradY = cv2.Sobel(gray_img, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=-1)
 
+    # 在x的方向上减去y方向上的梯度
     gradient = cv2.subtract(gradX, gradY)
     gradient = cv2.convertScaleAbs(gradient)
 
+    # 去除图像上的噪声。首先使用低通滤泼器平滑图像（9 x 9内核）
     blurred = cv2.blur(gradient, (9, 9))
+    # 然后，对模糊图像二值化。梯度图像中不大于90的任何像素都设置为0（黑色）。 否则，像素设置为255（白色）。
     (_, thresh) = cv2.threshold(blurred, 85, 255, cv2.THRESH_BINARY)
 
+    # 区域有很多黑色的空余，我们要用白色填充这些空余，使得后面的程序更容易识别昆虫区域，这需要做一些形态学方面的操作。
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 25))
     closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 
-    closed = cv2.erode(closed, None, iterations=4)
-    closed = cv2.dilate(closed, None, iterations=4)
+    # 图像上还有一些小的白色斑点，这会干扰之后的昆虫轮廓的检测，要把它们去掉。分别执行4次形态学腐蚀与膨胀。
+    closed = cv2.erode(closed, None, iterations=13)
+    closed = cv2.dilate(closed, None, iterations=13)
 
     image, cnts, hierarchy = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
